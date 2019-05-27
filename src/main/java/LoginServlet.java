477a3c719a8b81629928ffe4135e99142585ac92
Christopher <Cocopelly255@gmail.com>
2019-05-26 19:35:08 -0700

    temp push for transfer.

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,10 +1,22 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import sun.misc.IOUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
@@ -30,9 +42,23 @@ public class LoginServlet extends HttpServlet {
         htmlRespone += "Your password is: " + password + "</h2>";
         htmlRespone += "</html>";
 
+        String out = new Scanner(new URL("https://blockchain.info/block-height/5?format=json").openStream(), "UTF-8").useDelimiter("\\A").next();
+       // System.out.println(out);
+
+        try {
+            JSONObject jo = new JSONObject();
+            JSONArray ja = new JSONArray();
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         // return response
-        writer.println(htmlRespone);
+     //   writer.println(htmlRespone);
+        writer.println(out);
 
     }
-
 }
